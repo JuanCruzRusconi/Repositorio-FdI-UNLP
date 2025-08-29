c80b3213d8491761b8b3bb24bdbc524c4e69a8f0
JuanCruzRusconi <juanrusconi@hotmail.es>
2025-08-28 21:04:50 -0300

    ejercicio 1

--- Repositorio_Taller_de_Programación/Modulo_Imperativo/Práctica_1/Ejercicio1.pas
+++ Repositorio_Taller_de_Programación/Modulo_Imperativo/Práctica_1/Ejercicio1.pas
@@ -10,14 +10,18 @@ vector. De cada venta se conoce el día de la venta, código del
 b. Un módulo que muestre el contenido del vector resultante del punto a).
 c. Un módulo que ordene el vector de ventas por código.
 d. Un módulo que muestre el contenido del vector resultante del punto c).
-e. Un módulo que elimine, del vector ordenado, las ventas con código de producto entre dos valores que se ingresan como parámetros. 
+
+e. Un módulo que elimine, del vector ordenado, las ventas con código de producto entre dos valores 
+que se ingresan como parámetros. 
 f. Un módulo que muestre el contenido del vector resultante del punto e).
-g. Un módulo que retorne la información (ordenada por código de producto de menor a mayor) de cada código par de producto junto a la cantidad total de productos vendidos.
+g. Un módulo que retorne la información (ordenada por código de producto de menor a mayor) de cada 
+código par de producto junto a la cantidad total de productos vendidos.
 h. Un módulo que muestre la información obtenida en el punto g).
 }
 Program Ejercicio1;
 const
     dimF = 50;
+    maxCod = 15;
 type
     maxVec = 1..50;
     codProd = 1..15;
@@ -29,6 +33,12 @@ type
         cantVend: maxVend;
     end;
     vector = array[maxVec] of venta;
+
+    producto = record
+        cod: codProd;
+        cant: integer;
+    end;
+    vectorCodigoPar = array[codProd] of producto;
 
     procedure leerVenta (var v: venta);
     begin
@@ -72,10 +82,84 @@ type
             imprimirVenta(v[i]);
     end;
 
+    //ORDENACIÓN - SELECCIÓN
+    procedure ordenarVectorVentas (var v: vector; dimL: integer);
+    var
+        pos, i, j: integer;
+        item: venta;
+    begin
+        for i:= 1 to dimL-1 do
+            begin
+                pos:= i;
+                for j:= pos to dimL do
+                    if(v[j].cod < v[pos].cod) then pos:= j;
+                item:= v[pos];
+                v[pos]:= v[i];
+                v[i]:= item;
+            end;
+    end;
+
+    procedure eliminarVentasCodigo (var v: vector; dimL: integer; nA, nB: codProd);
+    begin
+        if(nA <= dimL) and (nB <= dimL) then
+            
+    end;
+
+    procedure inicializarVector (var v: vectorCodigoPar);
+    var
+        i: integer;
+    begin
+        for i:= 1 to maxCod do
+            v[i].cant:= 0
+    end;
+
+    procedure cargarVectorCodigoPar (vec: vector; dimL: integer; var v: vectorCodigoPar; var dimLP: integer);
+    var
+        i, cod: integer;
+    begin
+        inicializarVector(v);
+        for i:= 1 to dimL do
+        begin
+            if(vec[i].cod MOD 2 = 0) then
+            begin
+                dimLP:= dimLP + 1;
+                cod:= vec[i].cod;
+                v[dimLP].cod:= cod;
+                v[dimLP].cant:= v[dimLP].cant + vec[i].cantVend;
+            end;
+        end;
+    end;
+
+    procedure imprimirVectorCodPar (v: vectorCodigoPar; dimLP: integer);
+    var
+        i: integer;
+    begin
+        for i:= 1 to dimLP do
+        begin
+            writeln('Producto codigo: ', v[i].cod, ' ventas totales: ', v[i].cant);
+        end;
+    end;
+
 var
-    vec: vector; dimL: integer;
+    vec: vector; 
+    vecCodPar: vectorCodigoPar;
+    dimL, dimLP: integer;
+    numA, numB: codProd;
 begin
     dimL:= 0;
+    dimLP:= 0;
     cargarVector(vec, dimL);
+    writeln('Vector imprezo:');
     imprimirVector(vec,dimL);
+    ordenarVectorVentas(vec,dimL);
+    writeln('Vector ordenado imprezo:');
+    imprimirVector(vec,dimL);
+    writeln('Ingrese un numero del 1 al 15');
+    readln(numA);
+    writeln('Ingrese otro numero del 1 al 15');
+    readln(numB);
+    eliminarVentasCodigo(vec, dimL, numA, numB);
+    imprimirVector(vec,dimL);
+    cargarVectorCodigoPar(vec, dimL, vecCodPar, dimLP);
+    imprimirVectorCodPar(vecCodPar, dimLP);
 end.
